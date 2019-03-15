40b86c50c1b6c50136be81361316baf26e993587
LuizAlvesR <40923667+LuizAlvesR@users.noreply.github.com>
2019-03-15 12:48:29 -0300

    Adicionei o projeto ktharina
    Java 1.8 alterar.

---
+++ ktharina/target/generated-sources/annotations/br/com/ktharina/model/Member_.java
@@ -0,0 +1,21 @@
+package br.com.ktharina.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Member.class)
+public abstract class Member_ {
+
+	public static volatile SingularAttribute<Member, String> tipoElemento;
+	public static volatile SingularAttribute<Member, String> tipoEngaste;
+	public static volatile SingularAttribute<Member, String> phoneNumber;
+	public static volatile SingularAttribute<Member, String> projeto;
+	public static volatile SingularAttribute<Member, String> name;
+	public static volatile SingularAttribute<Member, String> tipoEstrutura;
+	public static volatile SingularAttribute<Member, Long> id;
+	public static volatile SingularAttribute<Member, String> email;
+
+}
+
